3415d21b33ea5e9b3f7ef4a2db94caeb95d7716d
Mariann <tapferm@gmail.com>
2020-07-29 13:37:19 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
-echo tere
 
-ls -lRm assets/img/pildid_proc/* > uusfail.txt
-
-
+# ls -lRm assets/img/pildid_proc/* > uusfail.txt
+node ./helpers/compile_pictures.js
 node ./node_modules/entu-ssg/src/serve.js ./entu-ssg.yaml full
