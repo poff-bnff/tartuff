d6f6b4cb395e0695e7d059f4b29fe84849516f7a
LiisKasper <68702810+LiisKasper@users.noreply.github.com>
2020-07-30 13:47:23 +0300

    pildid_proc to img_films

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 
-# ls -lRm assets/img/pildid_proc/* > uusfail.txt
+# ls -lRm assets/img/img_films/* > uusfail.txt
 node ./helpers/compile_pictures.js
 cp -R assets build/assets
 node ./node_modules/entu-ssg/src/serve.js ./entu-ssg.yaml full
