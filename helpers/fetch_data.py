deefd0117b9ff65784fd38f0c5e8c3c480b4622a
Mariann <tapferm@gmail.com>
2020-08-06 11:48:55 +0300

    YAML failides ankrute genereerimine välja lülitatud.
    Build läheb katki kui kasutame YAML-is ankruid. https://ttl255.com/yaml-anchors-and-aliases-and-how-to-disable-them/

--- helpers/fetch_data.py
+++ helpers/fetch_data.py
@@ -6,6 +6,12 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
+#lülitan välja automaatse YAML ankrute genereerimise
+class NoAliasDumper(yaml.SafeDumper):
+    def ignore_aliases(self, data):
+        return True
+
+
 os.chdir(os.path.dirname(__file__))
 
 def fetchData(sheetName, location, dataSources):
@@ -68,40 +74,57 @@ def fetchData(sheetName, location, dataSources):
                         count2 = 0
                         data = {}
                         for x in row:
+                            #siin saab PyYAML aru, et dataSource on iga kord sama ja asendab selle ankruga
                             if count2 == 0 and bool(dataSources) == True:
                                 data['data'] = dataSources
                             data[headers[count2]] = row[count2].strip()
                             count2 = count2 + 1
                         dict_file = dict_file + [data]
                         with open(r'../source/' + location, 'w', encoding='utf-8') as file:
-                            yaml.dump(dict_file, file, default_flow_style=False, sort_keys=False, indent=4, allow_unicode=True)
+                            yaml.dump(dict_file, file, default_flow_style=False, sort_keys=False, indent=4, allow_unicode=True, Dumper=NoAliasDumper)
+                            #print(yaml.safe_dump(interfaces))
+                            #print(yaml.dump(interfaces, Dumper=NoAliasDumper))
                 count = count + 1
     if __name__ == '__main__':
         main()
+
+
 """ siin kutsun välja üleval defineeritud funtiooni andes kaasa parameetritena:
 - sheetName = Google sheets lehekülje nimi kust infot loeme
 - location = kuhu fail genereeritakse ja mis selle nimi on (nt 'asukoht/failiNimi.yaml')
 - dataSources = dictionary tüüpi objekt lisatavatest failidest (nt {'pictures': '/film_pictures.yaml', 'screenings': 'screenings.yaml'})
     kui ei ole midagi vaja lisada  kirjuta {}
 """
-#fetchData('Artiklid', 'article/data.yaml', {'article_pictures': '/article_pictures.yaml'})
-#fetchData('art-et', 'article/data_et.yaml', {'article_pictures': '/article_pictures.yaml'})
-#fetchData('art-en', 'article/data_en.yaml', {'article_pictures': '/article_pictures.yaml'})
-#fetchData('art-ru', 'article/data_ru.yaml', {'article_pictures': '/article_pictures.yaml'})
+"""
+The issue with this is the use of aliases.
+Basically since the data I had in the ‘services’ dictionary was the same as what
+I had in parameters pyYAML recognized that and created the alias &id001 pointing at that data,
+then referenced it rather than copying that data into parameters.
 
-#fetchData('Events', 'events/data.yaml', {})
-#fetchData('events-et', 'events/data_et.yaml', {})
-#fetchData('events-en', 'events/data_en.yaml', {})
-#fetchData('events-ru', 'events/data_ru.yaml', {})
+class MyDumper(yaml.Dumper):
+def ignore_aliases(self, _data):
+return True
 
-#fetchData('Filmid', 'film/data.yaml', {'pictures': '/film_pictures.yaml', 'screenings': 'screenings.yaml'})
-#fetchData('filmid-et', 'film/data_et.yaml', {'pictures': '/film_pictures.yaml', 'screenings': 'screenings.yaml'})
-#fetchData('filmid-en', 'film/data_en.yaml', {'pictures': '/film_pictures.yaml', 'screenings': 'screenings.yaml'})
-#fetchData('filmid-ru', 'film/data_ru.yaml', {'pictures': '/film_pictures.yaml', 'screenings': 'screenings.yaml'})
 
-#fetchData('Seansid', 'film/screenings.yaml', {})
-#fetchData('seansid-et', 'film/screenings_et.yaml', {})
-#fetchData('seansid-en', 'film/screenings_en.yaml', {})
-#fetchData('seansid-ru', 'film/screenings_ru.yaml', {})
+"""
+fetchData('Artiklid', 'article/data.yaml', {'article_pictures': '/article_pictures.yaml'})
+fetchData('art-et', 'article/data_et.yaml', {'article_pictures': '/article_pictures.yaml'})
+fetchData('art-en', 'article/data_en.yaml', {'article_pictures': '/article_pictures.yaml'})
+fetchData('art-ru', 'article/data_ru.yaml', {'article_pictures': '/article_pictures.yaml'})
+
+fetchData('Events', 'events/data.yaml', {})
+fetchData('events-et', 'events/data_et.yaml', {})
+fetchData('events-en', 'events/data_en.yaml', {})
+fetchData('events-ru', 'events/data_ru.yaml', {})
+
+fetchData('Filmid', 'film/data.yaml', {'pictures': '/film_pictures.yaml', 'screenings': 'screenings.yaml'})
+fetchData('filmid-et', 'film/data_et.yaml', {'pictures': '/film_pictures.yaml', 'screenings': 'screenings.yaml'})
+fetchData('filmid-en', 'film/data_en.yaml', {'pictures': '/film_pictures.yaml', 'screenings': 'screenings.yaml'})
+fetchData('filmid-ru', 'film/data_ru.yaml', {'pictures': '/film_pictures.yaml', 'screenings': 'screenings.yaml'})
+
+fetchData('Seansid', 'film/screenings.yaml', {})
+fetchData('seansid-et', 'film/screenings_et.yaml', {})
+fetchData('seansid-en', 'film/screenings_en.yaml', {})
+fetchData('seansid-ru', 'film/screenings_ru.yaml', {})
 
 
