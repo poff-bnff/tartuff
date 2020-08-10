b53355ab71d7495567e9dcad6b45534acd4f056a
Mariann <tapferm@gmail.com>
2020-08-10 11:27:49 +0300

    Update calendar_view.py

--- helpers/calendar_view.py
+++ helpers/calendar_view.py
@@ -1,12 +1,9 @@
 import yaml
 import os
 
-# os.chdir(os.path.dirname(__file__))
+os.chdir(os.path.dirname(__file__))
 
-print("current working dir is: " + os.getcwd())
-print("current working dir is: " + os.getcwd() +"/source/film/screenings.yaml")
-
-yaml_file = open(os.getcwd() +"/source/film/screenings.yaml")
+yaml_file = open("../source/film/screenings.yaml")
 parsed_yaml_file = yaml.load(yaml_file, Loader=yaml.FullLoader)
 
 print(parsed_yaml_file)
