2aa56e0255aba200b9ff07bc4b18a90c2e7dbe80
Mariann <tapferm@gmail.com>
2020-08-10 15:25:38 +0300

    Update calendar_view.py

--- helpers/calendar_view.py
+++ helpers/calendar_view.py
@@ -12,23 +12,29 @@ parsed_yaml_file = yaml.load(yaml_file, Loader=yaml.FullLoader)
 # kuupäev selle all selle päeva seanside kinod ja selle all selle päeva selle kino kõik seansid ajalises järjekorras
 date=''
 calendarInfo=[]
+screeningPlace=''
 for val in sorted(parsed_yaml_file, key = lambda i: i['screeningDate']):
+
     if date != val["screeningDate"]:
         date=(val["screeningDate"])
         calendarInfo.append({'screeningDate' : date})
-        calendarInfo.append({'screeningCinema' : { val["screeningCinema"], {}}})
-    else:
+        screeningPlace=val["screeningCinema"]
+        calendarInfo.append({'screeningCinema' : {screeningPlace}})
 
 
-"""       cinema=''
-        for val in sorted(parsed_yaml_file, key = lambda i: i['screeningCinema']):
-            if cinema != val["screeningCinema"]:
-                cinema=(val["screeningCinema"])
-                print(calendarInfo)
-                print(cinema)
-                calendarInfo["screeningCinema"] = { "tartu kino", "pärnu kino"}
-"""
+
 print(calendarInfo)
+
+
+#     cinema=''
+ #       for val in sorted(parsed_yaml_file, key = lambda i: i['screeningCinema']):
+  #          if cinema != val["screeningCinema"]:
+ #               cinema=(val["screeningCinema"])
+#                print(calendarInfo)
+ #               print(cinema)
+ #               calendarInfo["screeningCinema"] = { "tartu kino", "pärnu kino"}
+#
+
 
 
 #cinema=''
@@ -36,13 +42,6 @@ print(calendarInfo)
 #    if cinema != val["screeningCinema"]:
 #        cinema=(val["screeningCinema"])
 
-thisdict =	{
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-thisdict["color"] = {"red", "blue"}
-print(thisdict)
 
 
 
