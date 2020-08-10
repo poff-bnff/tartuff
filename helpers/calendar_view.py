e3c6b22096aa3e5d6bafd382ce29dc9707d92c70
Mariann <tapferm@gmail.com>
2020-08-10 14:53:00 +0300

    Update calendar_view.py

--- helpers/calendar_view.py
+++ helpers/calendar_view.py
@@ -8,18 +8,33 @@ yaml_file = open("../source/film/screenings.yaml")
 parsed_yaml_file = yaml.load(yaml_file, Loader=yaml.FullLoader)
 
 #print(parsed_yaml_file)
-# print(parsed_yaml_file[0]["screeningEventivalId"])
-
+#print(parsed_yaml_file[0]["screeningEventivalId"])
+# kuupäev selle all selle päeva seanside kinod ja selle all selle päeva selle kino kõik seansid ajalises järjekorras
 date=''
 calendarInfo=[]
-#films: list of dictionarys
-#ScreeningCinema: list
 for val in sorted(parsed_yaml_file, key = lambda i: i['screeningDate']):
     if date != val["screeningDate"]:
         date=(val["screeningDate"])
         calendarInfo.append({'screeningDate' : date})
+        cinema=''
+        for val in sorted(parsed_yaml_file, key = lambda i: i['screeningCinema']):
+            if cinema != val["screeningCinema"]:
+                cinema=(val["screeningCinema"])
+                print(calendarInfo)
+                print(cinema)
+                calendarInfo["screeningCinema"] = cinema
 
 print(calendarInfo)
+
+
+#cinema=''
+#for val in sorted(parsed_yaml_file, key = lambda i: i['screeningCinema']):
+#    if cinema != val["screeningCinema"]:
+#        cinema=(val["screeningCinema"])
+
+
+
+
 
 
 
