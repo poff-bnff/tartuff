a42ce02d9b0bb7ac0ad7907ca371f363807fda00
Mariann <tapferm@gmail.com>
2020-08-10 11:21:02 +0300

    open path muudetud

--- helpers/calendar_view.py
+++ helpers/calendar_view.py
@@ -1,9 +1,12 @@
 import yaml
-import os.path
+import os
 
 # os.chdir(os.path.dirname(__file__))
 
-yaml_file = open("../source/film/screenings.yaml")
+print("current working dir is: " + os.getcwd())
+print("current working dir is: " + os.getcwd() +"/source/film/screenings.yaml")
+
+yaml_file = open(os.getcwd() +"/source/film/screenings.yaml")
 parsed_yaml_file = yaml.load(yaml_file, Loader=yaml.FullLoader)
 
 print(parsed_yaml_file)
