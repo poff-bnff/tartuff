21e35f0e4415e8fecdddba349a749e3f8c489ab8
Mariann <tapferm@gmail.com>
2020-08-11 17:13:13 +0300

    Update calendar_view.py

--- helpers/calendar_view.py
+++ helpers/calendar_view.py
@@ -16,19 +16,16 @@ def compileScreeningsCalendar (source, output):
     for s in screeningsData:
         #siin on määratud mitu tundi südaööst võib seansi algus üle olla, et arvestataks samasse päeva
         hours = -4
-        #google sheets date formaat pythonile mõistetavaks kuupäevaks ja kellaajaks
+        #google sheets date formaat pythonile mõistetavaks kuupäevaks ja kellaajak
+
         myBeforeDateTime = (datetime.strptime(s['screeningDatetime'], '%Y-%m-%dT%H:%M:%S%z') + timedelta(hours=hours))
         #eraldame kuupäevast ja kellaajast kuu ja päeva
         myBeforeDate = "d"+ str(myBeforeDateTime.date())
-        print(myBeforeDate)
-
-
         myDate = calendarData.get(myBeforeDate, dict())
         myCinema = myDate.get(s['screeningCinema'], [])
-        myCinema.append({'screeningDatetime': s['screeningDatetime'],'dateTimeForSorting': myBeforeDateTime, 'screeningTime': s['screeningTime'], 'filmTitle': s['filmTitle'], 'filmPath': s['filmPath']})
+        myCinema.append({'screeningDatetime': s['screeningDatetime'],'dateTimeForSorting': myBeforeDateTime, 'filmTitle': s['filmTitle'], 'filmPath': s['filmPath']})
 
-        myCinemaSorted = sorted(myCinema, key = lambda i: (i['dateTimeForSorting']))
-        myDate[s['screeningCinema']] = myCinemaSorted
+        myDate[s['screeningCinema']] = sorted(myCinema, key = lambda i: (i['dateTimeForSorting']))
         calendarData[myBeforeDate] = myDate
 
     with open(r'../source/film/'+output, 'w', encoding='utf-8') as file:
@@ -37,6 +34,6 @@ def compileScreeningsCalendar (source, output):
     print("compiling " + output)
 
 #compileScreeningsCalendar('screenings.yaml', 'screeningsCalendar.yaml')
-#compileScreeningsCalendar('screenings.en.yaml', 'screeningsCalendar.en.yaml')
+compileScreeningsCalendar('screenings.en.yaml', 'screeningsCalendar.en.yaml')
 compileScreeningsCalendar('screenings.et.yaml', 'screeningsCalendar.et.yaml')
 
