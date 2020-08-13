4cdcdda304e5b99433e78f48902cc4a19f4f75ab
Mariann <tapferm@gmail.com>
2020-08-13 15:04:45 +0300

    compile_calendar_data.py sees funktsioon ja selle väljakutsumisel loodud vajalikud failid film kausta

--- helpers/compile_calendar_data.py
+++ helpers/compile_calendar_data.py
@@ -10,8 +10,6 @@ class NoAliasDumper(yaml.SafeDumper):
 
 os.chdir(os.path.dirname(__file__))
 
-#data = sorted(parsed_yaml_file, key = lambda i: (i['screeningDate'], i['screeningCinema'], i['screeningTime']))
-
 #funktsioonid, et teha string kus on dateTime py-le arusaadavaks dateTime-iks ja date-iks
 def formatDateTime(dateTime):
     return str(datetime.strptime(dateTime, '%Y-%m-%dT%H:%M:%S%z'))
@@ -19,47 +17,54 @@ def formatDateTime(dateTime):
 def formatedDate(dateTime):
     return str((datetime.strptime(dateTime, '%Y-%m-%dT%H:%M:%S%z')).date())
 
-yaml_file = open("../source/film/screenings.et.yaml", encoding='utf-8')
-screeningsData = yaml.load(yaml_file, Loader=yaml.FullLoader)
 
-calendarDays = []
-#screeningData on list of dictionarys
-for screening in screeningsData:
-    #screening on dictionary tüüpi
-    oneDay = []
-    for item in calendarDays:
-        if item['calendarDate'] == formatedDate(screening['calendarDateTime']):
-            #item=[{'calendarDate': '2020-08-10'},]
-            oneDay = item
-    if oneDay == []:
-        calendarDays.append({'calendarDate': formatedDate(screening['calendarDateTime']), 'cinemas': []})
-##KAS CALENDARDAY-s PEAKS SORTEERIMA??
 
-#otsin screeningCinemad kuupäeva kaupa välja ja panen objekti sisse
-cinemas = []
-#see funktsioon, mis määrab, mis järjekorras sorteerime
-def keyForSortingCinemas(c):
-    sortOrder={"Athena Keskus":0, "Raekoja plats":1, "Elisa Stage":2}
-    return sortOrder[c['cinema']]
+def compileCalData(input, output):
+    yaml_file = open("../source/film/"+input, encoding='utf-8')
+    screeningsData = yaml.load(yaml_file, Loader=yaml.FullLoader)
 
-for day in calendarDays:
+    calendarDays = []
+    #screeningData on list of dictionarys
     for screening in screeningsData:
-        if day['calendarDate'] == formatedDate(screening['calendarDateTime']):
-            cinemas.append({'cinema': screening['screeningCinema'], 'screenings': []})
-#siin panen cinemas listi day objekti sisse õigesse kohta
-#SIIN TAHAN SORTEERIDA
-    uniqueCinemas = list({v['cinema']:v for v in cinemas}.values())
+        #screening on dictionary tüüpi
+        oneDay = []
+        for item in calendarDays:
+            if item['calendarDate'] == formatedDate(screening['calendarDateTime']):
+                #item=[{'calendarDate': '2020-08-10'},]
+                oneDay = item
+        if oneDay == []:
+            calendarDays.append({'calendarDate': formatedDate(screening['calendarDateTime']), 'cinemas': []})
+    ##KAS CALENDARDAY-s PEAKS SORTEERIMA??
 
-    print(sorted(uniqueCinemas, key = keyForSortingCinemas ))
-    day['cinemas']=sorted(uniqueCinemas, key = keyForSortingCinemas )
+    #otsin screeningCinemad kuupäeva kaupa välja ja panen objekti sisse
+    cinemas = []
+    #see funktsioon, mis määrab, mis järjekorras sorteerime
+    def keyForSortingCinemas(c):
+        sortOrder={"Athena Keskus":0, "Raekoja plats":1, "Elisa Stage":2}
+        return sortOrder[c['cinema']]
 
-#käin läbi kõik screeningud screenings.yaml failis
-for screening in screeningsData:
-    [day] = [d for d in calendarDays if d['calendarDate'] == formatedDate(screening['calendarDateTime'])]
-    [cinema] = [c for c in day['cinemas'] if c['cinema'] == screening['screeningCinema']]
-    cinema['screenings'].append({'filmTitle': screening['filmTitle'], 'screeningTime' : screening['screeningTime'], 'filmPath': screening['filmPath'] })
+    for day in calendarDays:
+        for screening in screeningsData:
+            if day['calendarDate'] == formatedDate(screening['calendarDateTime']):
+                cinemas.append({'cinema': screening['screeningCinema'], 'screenings': []})
+    #siin eemaldan cinemas listist duplikaadid, sorteerin ja  panen cinemas listi day objekti sisse õigesse kohta
+        uniqueCinemas = list({v['cinema']:v for v in cinemas}.values())
+        day['cinemas']=sorted(uniqueCinemas, key = keyForSortingCinemas )
 
-with open(r'TESTbetter.yaml', 'w', encoding='utf-8') as file:
-    yaml.dump(calendarDays, file, default_flow_style=False, sort_keys=True, indent=4, allow_unicode=True, Dumper=NoAliasDumper)
+    #käin läbi kõik screeningud screenings.yaml failis
+    for screening in screeningsData:
+        [day] = [d for d in calendarDays if d['calendarDate'] == formatedDate(screening['calendarDateTime'])]
+        [cinema] = [c for c in day['cinemas'] if c['cinema'] == screening['screeningCinema']]
+        cinema['screenings'].append({'filmTitle': screening['filmTitle'], 'screeningTime' : screening['screeningTime'], 'filmPath': screening['filmPath'] })
+
+    with open(r'../source/'+output, 'w', encoding='utf-8') as file:
+        yaml.dump(calendarDays, file, default_flow_style=False, sort_keys=True, indent=4, allow_unicode=True, Dumper=NoAliasDumper)
+    print('compiling ' + output)
+
+
+## KUTSUN VÄLJA FUNKTSIOONI
+compileCalData('screenings.et.yaml','film/screeningsCalendar.et.yaml')
+compileCalData('screenings.en.yaml','film/screeningsCalendar.en.yaml')
+
 
 
