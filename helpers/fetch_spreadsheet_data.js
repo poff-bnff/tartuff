7f6006d45adfa3e47ff2ba2906d627b32e57575e
LiisKasper <68702810+LiisKasper@users.noreply.github.com>
2020-08-17 12:09:28 +0300

    update

--- helpers/fetch_spreadsheet_data.js
+++ helpers/fetch_spreadsheet_data.js
@@ -2,16 +2,17 @@ const fs = require('fs');
 const readline = require('readline');
 const {google} = require('googleapis');
 
+
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = '/helpers/token.json';
+const TOKEN_PATH = 'token.json';
 
 // Load client secrets from a local file.
 
-fs.readFile('helpers/credentials.json', (err, content) => {
+fs.readFile('credentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
   // Authorize a client with credentials, then call the Google Sheets API.
   authorize(JSON.parse(content), fetchDataFromSheet);
